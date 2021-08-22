c2185bc59025cdd9a55252f7ea078febb37eafa1
Omar Khaled <45782982+omar39@users.noreply.github.com>
2021-08-22 16:44:34 +0200

    Initial commit-Assets setup

---
+++ NVC game/Assets/ArabicSupport/ArabFix.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ArabicSupport;
+using UnityEngine.UI;
+
+public class ArabFix : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text _text;
+    void Start()
+    {
+        _text = GetComponent<Text>();
+        _text.text = ArabicFixer.Fix(_text.text);
+    }
+    
+
+}
